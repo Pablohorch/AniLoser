1c507994c1bbc24ed32e8b2ed79e5e26e69a59f9
pablohorch <thexd1997@gmail.com>
2018-05-08 16:29:34 +0200

    200 millones de variables

--- app/src/main/java/paquete/horch/aniloser/listados.java
+++ app/src/main/java/paquete/horch/aniloser/listados.java
@@ -3,5 +3,15 @@ package paquete.horch.aniloser;
 public class listados {
     static String[] especies={"Perro","Gato","Pajaro Domestico","Caballo","Huron","Serpiente","Camaleon","Tortuga"};
 
-    String[] razaPerros={"No lo se","YorkShire","Pomerania","Caniche",""};
+    String[] razaPerros={"No lo se","Mestizo (chucho)","YorkShire","Pomerania","Caniche","Pastor Aleman","Labrador","Rottweiler","Bulldog","Gran Danes","Doberman",
+            "Perro Salchicha","Husky Siberiano","Boxer","Chihuahua","Pug (nariz acahatada)","Chow Chow","Mastin Ingles","Border Collie","Vizsla","Shih Tzu",
+            "Bichon Maltes","Pastro Belga","PitBull","Galgo Ingles","Akita","Pastor Ovejero","San Bernado","Terranova","BobTail","Bodegero","BullDog France",
+            "Bichon Frise","Bull Terrier","Staffordshire bull terrier"};
+    String[] razaGato={"No lo se","Siames","Persa","RagDoll","Bengala","Sphynx","Abisinio","Azul Ruso","Burmes","Bobtail Americano","Gato Exotico","Amercian shortHair",
+                        "Scottish Fold","Siberiano","Sagrado Birnamita","Ocicat","Bosque de Noruega","Tonkines","Manx","Devon Rex","Angora Turco","Savannah",
+                        "Mau egipcio","Curl Americano"};
+    String[] razaPajaros={"Loros","Canarios","Golondriana","Inseparable","Diamante Mandarin","Periquito"};
+
+    String [] serpiente={"Cascabel",""};
+
 }
